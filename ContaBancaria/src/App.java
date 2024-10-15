fb78844e31f6fca128484850c558ecf08f5a6a82
José Olmedilha <joseolmedilha@hotmail.com>
2024-10-14 23:26:15 -0300

    Criação da classer APP

--- ContaBancaria/src/App.java
+++ ContaBancaria/src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public static void main(String[] args) throws Exception {
+      
+    Scanner sc = new Scanner(System.in);
+
+    int numeroConta;
+    String numeroAgencia;
+    String nomeCliente;
+    Double saldoConta;
+
+    System.out.println("Digite o numero da conta: ");
+    numeroConta = sc.nextInt();
+    sc.nextLine();
+
+    System.out.println("Digite o numero da agência: ");
+    numeroAgencia = sc.nextLine();
+
+    System.out.println("Digite o seu nome: ");
+    nomeCliente = sc.nextLine();
+       
+
+    sc.close();
+
     }
 }
